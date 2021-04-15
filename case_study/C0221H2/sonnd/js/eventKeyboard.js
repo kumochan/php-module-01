425568c8945ccc446898455ed2f76e6c8c1cd639
kumochan <thaotrinhminh@gmail.com>
2021-04-15 16:40:08 +0700

    update folder

--- case_study/C0221H2/sonnd/js/eventKeyboard.js
+++
@@ -1,9 +0,0 @@
-const key = [];
-window.addEventListener('keydown', function (e){
-    key[e.keyCode] = true;
-    player.moving = true;
-})
-window.addEventListener('keyup', function (e){
-    delete key[e.keyCode];
-    player.moving = false;
-})
